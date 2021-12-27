75d93c47e60169376dc26a3e260093d985111e50
luuiz11 <94998850+luuiz11@users.noreply.github.com>
2021-12-26 23:15:34 -0300

    Add files via upload

---
+++ website/paginaphp.php
@@ -0,0 +1,65 @@
+<?php 
+$paginas =['home'=>'Minha página home aqui','sobre'=>'Estou na página sobre','contato'=>'<input type="text" />'];
+?>
+<!DOCTYPE html>
+	<html>
+	<head>
+	<title>website</title>
+	<style type="text/css">{
+margin: 0;
+padding: 0;
+box-sizing: border-box;
+	}
+	header{
+		background-color: #069;
+		padding: 8px 10px;
+		text-align: center;
+	}
+	a{
+		display: inline-block;
+		margin: 0 10px;
+		color: white;
+		font-size: 17px;
+	}
+	section{
+		max-width: 960px;
+		margin: 20px auto;
+		padding: 0 2%;
+	}
+	h2{
+		background-color: #069;
+		color white;
+		padding: 8px 10px;
+
+	}
+	p{
+		color: black;
+		margin-top: 10px;
+		font-size: 16px;
+	}
+</style>
+</head>
+<body>
+<header>
+	<?php
+	foreach ($paginas as $key => $value) {
+		echo '<a href="?page='.$key.'">'.ucfirst($key).'</a>';
+		}
+	?>
+</header>
+<section>
+	<h2><?php
+$pagina =(isset($_GET['page'])?$_GET['page']:'home');
+
+
+if (!array_key_exists($pagina, $paginas)){
+	$pagina= 'home';
+}
+
+echo ucfirst($pagina);
+
+?></h2>
+<p><?php echo $paginas [$pagina]; ?></p>
+</section>
+</body>
+</html>
